907034b1f98b465d16d2d138db39ad9ce5ee2168
enomoto11 <t.enomoto@catallaxy.me>
2023-05-07 18:06:35 +0900

    デバッグ処理を追加

--- main.cpp
+++ main.cpp
@@ -43,13 +43,16 @@ int main()
   const double dt = 1.0e-03;                                   // 時間刻み[s]
   const double DELTA_X = L / ELEMENT_NUM;                      // 要素の長さ[m]
   const double PI = M_PI;                                      // 円周率
-  const double h0 = 1.0e-03;                                   // 初期状態のtubeの厚さ[m]
+  const double r0 = 1.0e-05;                                   // 初期状態のtubeの半径[m]
+  const double h0 = 1.0e-06;                                   // 初期状態のtubeの厚さ[m]
+  const double A0 = PI * pow(r0 - h0, 2.0e0);                  // 初期状態のtubeの流路面積（位置座標によらない）[m^2]
   const double K_R = 3.3e-06;                                  // 粘性抵抗K_R[m^2/s]
-  const double rho = 1060000;                                  // 密度[g/m^3]
+  const double rho = 1060000.0;                                // 密度[g/m^3]
   const double E = 1.0e08;                                     // ヤング率(0.1MPa)[g/m/s^2]
-  const double A0 = PI * pow(9.0e-03, 2.0e0);                  // 初期状態のtubeの流路面積（位置座標によらない）[m^2]
   const double betha = 4.0e0 / 3.0e0 * sqrt(PI) * h0 * E / A0; // ß
   // const double dbetha_dx = -4.0 / 3.0 * h0 * sqrt(PI) / PI / PI * E * 1.0e8; // dß/dx
+
+  double v0 = 1.0e-2; // 所定位置における初期状態のtubeの流速[m/s]
 
   std::vector<std::vector<double>> node(NODE_NUM, std::vector<double>(3, 0));
   std::ifstream ifs("input/node_d.dat"); // ファイル入力
@@ -92,7 +95,7 @@ int main()
     // 初期状態のnodeに与える速度はノード点の位置によって変える
     if (i >= 0 && i <= NODE_NUM / 2)
     {
-      velocity[0][i] = 1.0e0;
+      velocity[0][i] = v0;
     }
     flowQuantity[0][i] = area[0][i] * velocity[0][i];
   }
@@ -149,15 +152,15 @@ int main()
       b_flowQuantity(ele1) = b_flowQuantity(ele1) - dt * (K_R * DELTA_X / 6.0e0 * (1.0e0 * (flowQuantity1 / area1) + 2.0e0 * (flowQuantity2 / area2)) + dt / 2.0e0 * pow(K_R, 2.0e0) * DELTA_X / 6.0e0 * (1.0e0 * (flowQuantity1 / pow(area1, 2.0e0)) + 2.0e0 * (flowQuantity2 / pow(area2, 2.0e0))));
     }
 
-    for (int j = 0; j < ELEMENT_NUM + 1; j++)
-    {
-      A_area(0, j) = 0.0e0;
-      A_flowQuantity(0, j) = 0.0e0;
-    }
-    A_area(0, 0) = 1.0e0;
-    A_flowQuantity(0, 0) = 1.0e0;
-    b_area(0) = 0.0e0;
-    b_flowQuantity(0) = 0.0e0;
+    // for (int j = 0; j < ELEMENT_NUM + 1; j++)
+    // {
+    //   A_area(0, j) = 0.0e0;
+    //   A_flowQuantity(0, j) = 0.0e0;
+    // }
+    // A_area(0, 0) = 1.0e0;
+    // A_flowQuantity(0, 0) = 1.0e0;
+    // b_area(0) = 0.0e0;
+    // b_flowQuantity(0) = 0.0e0;
 
     Eigen::VectorXd x_area = VectorXd::Zero(ELEMENT_NUM + 1);
     Eigen::VectorXd x_flowQuantity = VectorXd::Zero(ELEMENT_NUM + 1);
@@ -165,6 +168,7 @@ int main()
     // 行列計算
     x_area = A_area.fullPivLu().solve(b_area);
     x_flowQuantity = A_flowQuantity.fullPivLu().solve(b_flowQuantity);
+
     for (int j = 0; j < x_area.size(); j++)
     {
       area[i + 1][j] = x_area(j);
@@ -173,6 +177,50 @@ int main()
     {
       flowQuantity[i + 1][j] = x_flowQuantity(j);
     }
+
+    // TODO: debugを関数化したい
+    // --------------------------------------------------------------------debug
+    // b_areaの出力
+    std::ostringstream oss_b_area;
+    oss_b_area << "debug/b/area/debug" << i << ".csv";
+    std::ofstream outputfile_b_area(oss_b_area.str());
+    for (int j = 0; j < ELEMENT_NUM + 1; j++)
+    {
+      outputfile_b_area << b_area(j) << std::endl;
+    }
+    outputfile_b_area.close();
+
+    // b_flowQuantityの出力
+    std::ostringstream oss_b_flowQuantity;
+    oss_b_flowQuantity << "debug/b/flowQuantity/debug" << i << ".csv";
+    std::ofstream outputfile_b_flowQuantity(oss_b_flowQuantity.str());
+    for (int j = 0; j < ELEMENT_NUM + 1; j++)
+    {
+      outputfile_b_flowQuantity << b_flowQuantity(j) << std::endl;
+    }
+    outputfile_b_flowQuantity.close();
+
+    // x_areaの出力
+    std::ostringstream oss_x_area;
+    oss_x_area << "debug/x/area/debug" << i << ".csv";
+    std::ofstream ofs_x_area(oss_x_area.str());
+    for (int i = 0; i < x_area.size(); i++)
+    {
+      ofs_x_area << x_area(i) << std::endl;
+    }
+    ofs_x_area.close();
+
+    // x_flowQuantityの出力
+    std::ostringstream oss_x_flowQuantity;
+    oss_x_flowQuantity << "debug/x/flowQuantity/debug" << i << ".csv";
+    std::ofstream ofs_x_flowQuantity(oss_x_flowQuantity.str());
+    for (int i = 0; i < x_flowQuantity.size(); i++)
+    {
+      ofs_x_flowQuantity << x_flowQuantity(i) << std::endl;
+    }
+    ofs_x_flowQuantity.close();
+
+    // --------------------------------------------------------------------debug
   }
 
   int d = 100;
