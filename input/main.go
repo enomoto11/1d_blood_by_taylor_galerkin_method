3c22b048555c24c7635ee070ee8386f55787afc6
enomoto11 <t.enomoto@catallaxy.me>
2023-05-07 16:41:31 +0900

    要素分割を100に拡大

---
+++ input/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+const (
+	ElementD = 100 // ElementD is the number of elements in the D matrix
+)
+
+func outputElement_d() {
+	file, err := os.Create("./element_d.dat")
+	if err != nil {
+		fmt.Println("Error creating file:", err)
+		return
+	}
+	defer file.Close()
+
+	for i := 0; i < ElementD; i++ {
+		fmt.Fprintf(file, "%d %d\n", i, i+1)
+	}
+}
+
+func outputNode_d() {
+	file, err := os.Create("./node_d.dat")
+	if err != nil {
+		fmt.Println("Error creating file:", err)
+		return
+	}
+	defer file.Close()
+
+	for i := 0; i <= ElementD; i++ {
+		var x float32
+		x = float32(i) / float32(ElementD)
+		fmt.Fprintf(file, "%.2f %d %d\n", x, 0, 0)
+	}
+}
+
+func main() {
+	outputElement_d()
+	outputNode_d()
+}
