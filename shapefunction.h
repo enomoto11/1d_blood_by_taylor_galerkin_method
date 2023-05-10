c086c2a9eafa2402a9c3fdc84c4f03e67fcc6356
enomoto11 <t.enomoto@catallaxy.me>
2023-05-10 10:58:41 +0900

    名前空間を記述

--- shapefunction.h
+++ shapefunction.h
@@ -1,25 +1,25 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
 
+using namespace std;
 
-
-class ShapeFunction1D{
- public:
-
-  static void P2_N(vector<double> &N,const double &g1)
+class ShapeFunction1D
+{
+public:
+  static void P2_N(vector<double> &N, const double &g1)
   {
-    N.at(0) = 5e-1*(1e0-g1);
-    N.at(1) = 5e-1*(1e0+g1);
+    N.at(0) = 5e-1 * (1e0 - g1);
+    N.at(1) = 5e-1 * (1e0 + g1);
   }
-  static void P2_dNdr(veector<double> &dNdr,const double &g1)
+  static void P2_dNdr(vector<double> &dNdr, const double &g1)
   {
     dNdr.at(0) = -5e-1;
     dNdr.at(1) = 5e-1;
   }
 
-  static void P2_dNinvdr(vector<double> &dNinvdr,const double &g1)
+  static void P2_dNinvdr(vector<double> &dNinvdr, const double &g1)
   {
-    dNinvdr.at(0) = 5e-1*(1e0-g1)*(1e0-g1);
-    dNinvdr.at(1) = 5e-1*(1e0+g1)*(1e0+g1);
+    dNinvdr.at(0) = 5e-1 * (1e0 - g1) * (1e0 - g1);
+    dNinvdr.at(1) = 5e-1 * (1e0 + g1) * (1e0 + g1);
   }
-
-}
+};
