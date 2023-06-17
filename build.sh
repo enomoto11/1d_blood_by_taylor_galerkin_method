315f6f54273d125741532fc46aa7536244068b6c
enomoto11 <t.enomoto@catallaxy.me>
2023-06-17 13:46:16 +0900

    intelコンパイラーを使わずにビルドするように修正

--- build.sh
+++ build.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 mkdir build
 cd build
-cmake -D compiler=intel \
-      -D enable_GLOG=ON \
-      ..
-
-      # -D CMAKE_INSTALL_PREFIX=/home/totani/bin/BDIM-cpp \
-      # -D TP_DIR=/home/totani/lib/TextParser-1.8.5 \
-      # -D enable_GLOG=ON \
-      # -D GLOG_DIR=/home/totani/lib/glog \
+cmake -D enable_GLOG=OFF  -D EIGEN_DIR=/Users/t-enomoto/research/1d_blood/lib/eigen -D EIGEN_INCLUDE_DIR=/Users/t-enomoto/research/1d_blood/lib/eigen ..
 make && make install
